83a3d8367db90a4a8ee454679f1de1eee78be7f9
EdePB <30841280+EdePB@users.noreply.github.com>
2017-08-21 23:58:30 -0500

    Delete Practica 2.R

--- P2/Practica 2.R
+++
@@ -1,9 +0,0 @@
-  for(prb in 1:9){
-  p=prb/10
-  actual <- matrix(sample(c(0,1),num,prob=c(p,p), replace=TRUE), nrow=dim, ncol=dim)
-  suppressMessages(library("sna"))
-  png("p2_t0.png")
-  plot.sociomatrix(actual, diaglab=FALSE, main="Inicio")
-  graphics.off()
-  print(p)
-  }
